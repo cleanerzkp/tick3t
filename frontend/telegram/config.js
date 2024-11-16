24a5fc5dead6a8adc0f84ef61331426382443762
HemangVora <hemangvora2@gmail.com>
2024-11-16 21:27:06 +0700

    telegram mini bot config added

--- frontend/telegram/config.js
+++ frontend/telegram/config.js
@@ -3,14 +3,17 @@ require("dotenv").config();
 const config = {
   // Bot configuration
   TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
-  LOGIN_URL: process.env.LOGIN_URL,
+  WEBAPP_URL: process.env.WEBAPP_URL,
+  IMAGE_PATH: "./image.png",
+  TICK3T_APP_URL: "t.me/tick3tapp_bot/tick3t_app",
+  TICK3T_APP_URL_CREATE: "t.me/tick3tapp_bot/maybee_app_create",
+  TICK3T_APP_URL_JOIN: "t.me/tick3tapp_bot/maybee_app_join",
 
-  // Channel and topic IDs
-  CHANNEL_ID: "@",
+  CHANNEL_ID: "@tick3tapp_bot",
 };
 
 // Validate required configuration
-const requiredConfigs = ["TELEGRAM_BOT_TOKEN", "LOGIN_URL"];
+const requiredConfigs = ["TELEGRAM_BOT_TOKEN", "WEBAPP_URL"];
 for (const configName of requiredConfigs) {
   if (!config[configName]) {
     console.error(`Missing required configuration: ${configName}`);
